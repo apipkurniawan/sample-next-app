d668a6b27915d625213b126457c095c58127836c
apipkurniawan <davitaja92@gmail.com>
2025-01-12 11:57:34 +0700

    feat: incremental-site-rendering

---
+++ src/pages/isr/index.tsx
@@ -0,0 +1,67 @@
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+};
+
+const myLoader = ({
+  src,
+  width,
+  quality,
+}: {
+  src: string;
+  width: number;
+  quality?: number;
+}) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
+
+interface ProductsPageProps {
+  products: Product[];
+}
+
+const ProductsPage = ({ products }: ProductsPageProps) => {
+  return (
+    <div className='container mx-auto px-4 py-8'>
+      <h1 className='text-2xl font-bold mb-6'>Product List</h1>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {products.map((product) => (
+          <div key={product.id} className='border p-4 rounded-lg shadow-lg'>
+            <Image
+              src={product.image}
+              alt={product.name}
+              className='w-full h-40 object-cover mb-4 rounded-lg'
+              width={160}
+              height={160}
+              loader={myLoader}
+            />
+            <h2 className='text-xl font-semibold'>{product.name}</h2>
+            <p className='text-gray-700'>{product.description}</p>
+            <p className='text-lg font-bold text-green-600'>${product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// Fetch products with ISR
+export const getStaticProps: GetStaticProps = async () => {
+  // Fetching data from a dummy API
+  const res = await fetch('https://fakestoreapi.com/products');
+  const products = await res.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 10, // Regenerate page every 10 seconds
+  };
+};
+
+export default ProductsPage;
